1e654015b8ec2891cc7f51b2178b982ac383acd5
Shadrackbond <145914322+shadrackbond@users.noreply.github.com>
2026-09-22 09:50:48 +0300

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        for(let i =0; i<numbers.length;i++){
+            for(let x = 0; x<numbers.length;x++){
+                if(i===x){
+                    x++;
+                    if(i+x === target){
+                        return [i,x]
+                    }
+                }
+            }
+
+        }
+    }
+}
